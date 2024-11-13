cba603210c794176fd02042af2501010a3cabb6c
mitugui <cristian.mitugui04@gmail.com>
2024-11-13 17:00:05 -0300

    refactor: update GET endpoint to list doctors with pagination using Pageable - Modified MedicoController GET /medicos endpoint to support paginated responses, utilizing Spring's Pageable. - Configured default pagination with a page size of 10 and ascending order by nome field. - Updated method to return a Page of DadosListagemMedico DTO, allowing efficient and organized retrieval of doctor records.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,16 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
-import med.voll.api.medico.DadosCadastroMedico;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -24,7 +25,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar() {
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
