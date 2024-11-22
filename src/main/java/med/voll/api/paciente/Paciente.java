5b192ee1388d6105166fb84e253fd6b19545bfc0
mitugui <cristian.mitugui04@gmail.com>
2024-11-21 21:14:30 -0300

    feat: add POST endpoint to register patient - Mapped to /pacientes. - Created Paciente class with JPA annotations to define the patient entity. - Created DadosCadastroPaciente DTO to encapsulate data from the request body for patient registration. - Created PacienteRepository to handle database operations by extending JpaRepository. - Created PacienteController to handle HTTP requests related to patients. - Implemented POST method in PacienteController to register new patients.

---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,38 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Entity(name = "Paciente")
+@Table(name = "pacientes")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    String cpf;
+    String telefone;
+
+    @Embedded
+    Endereco endereco;
+
+    Boolean ativo;
+
+    public Paciente(DadosCadastroPaciente dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.cpf = dados.cpf();
+        this.telefone = dados.telefone();
+        this.endereco = new Endereco(dados.endereco());
+        ativo = true;
+    }
+}
