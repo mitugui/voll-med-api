911dfaa0b3472c7d9bf4c1023ad0da4373880476
mitugui <cristian.mitugui04@gmail.com>
2024-11-23 20:40:54 -0300

    feat: add GET endpoint to retrieve patient details - Mapped to /{id}. - Implemented detalhar method to fetch patient details by ID in PacienteController. - Created and used DadosDetalhamentoPaciente DTO to structure the response.

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,14 +2,12 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosDetalhamentoPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -22,5 +20,12 @@ public class PacienteController {
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
        var paciente = new Paciente(dados);
        repository.save(paciente);
+    }
+
+    @GetMapping("/{id}")
+    public DadosDetalhamentoPaciente detalhar(@PathVariable Long id) {
+        var paciente  = repository.getReferenceById(id);
+
+        return new DadosDetalhamentoPaciente(paciente);
     }
 }
