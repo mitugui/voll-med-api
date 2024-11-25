fb8332feef448dd49573a38ced073af8c61c0482
mitugui <cristian.mitugui04@gmail.com>
2024-11-25 08:30:54 -0300

    feat: add GET endpoint to list patients with pagination and create DadosListagemPaciente DTO - Mapped to /pacientes endpoint. - Created DadosListagemPaciente record to structure doctor listing data. - Configured default pagination with a page size of 10 and ascending order by nome field. - Implemented method to return a Page of DadosListagemPaciente, ensuring efficient and well-structured retrieval of patient records.

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,11 +1,11 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.paciente.DadosCadastroPaciente;
-import med.voll.api.paciente.DadosDetalhamentoPaciente;
-import med.voll.api.paciente.Paciente;
-import med.voll.api.paciente.PacienteRepository;
+import med.voll.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -32,5 +32,12 @@ public class PacienteController {
     public ResponseEntity detalhar(@PathVariable Long id) {
         var paciente  = repository.getReferenceById(id);
         return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemPaciente>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        var pagina = repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+
+        return ResponseEntity.ok(pagina);
     }
 }
